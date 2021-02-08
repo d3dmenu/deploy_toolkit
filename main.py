7086ec046e5971ebd7f61fd16bf703e76a2ff5e0
Admin <tinnapat.r@ku.th>
2021-02-08 23:18:39 +0700

    Initial commit

--- main.py
+++ main.py
@@ -1,41 +1,10 @@
-import uvicorn 
-import numpy as np
-import warnings
-
-from fastapi import FastAPI, Form   
+from fastapi import FastAPI
 
 app = FastAPI()
-warnings.simplefilter("ignore")
+
+#domain where this api is hosted for example : localhost:5000/docs to see swagger documentation automagically generated.
+
 
 @app.get("/")
-async def main():
-    return 'Deploy API Success'
-
-@app.get("/calc")
-async def calculator(a : int = 0, b : int = 0):
-    return {"result": a + b}
-
-@app.post("/login/")
-async def login(username: str = Form(...), password: str = Form(...)):
-    return {"username": username, "password": password}
-
-@app.post("/data/")
-async def visual(data: list = Form(...), account: str = Form(...), firebase: str = Form(...)):
-    data = np.array(data).astype(float)
-    count, status = 0, False
-    for i in range(100, len(data)):
-        
-        PosX = data[i - 100:i].mean()
-        PosY = data[i - 200:i].mean()
-        PosZ = abs(PosX - PosY)
-
-        if PosX > PosY and status == False:
-            count += 1
-            status = True
-        if PosX < PosY and status == True:
-            status = False
-    return {"size": len(data), "count": count-1, "account": account, "firebase": firebase}
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host = "127.0.0.1", port = 8080, debug=True)
-
+def home():
+    return {"message":"Hello TutLinks.com"}
